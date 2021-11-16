9328faf220cb1b078bad1c5893b2203aa6dc80f3
Awanit Ranjan <56031024+Awanit512@users.noreply.github.com>
2021-11-16 19:15:18 +0530

    bss0 and bss1 in diff ssid
    updated code to have bss0 and bss1 in different network

--- scratch/TCP-Ledbat-Evaluation/wifi-tcp-ledbat_flexible_dumbell.cc
+++ scratch/TCP-Ledbat-Evaluation/wifi-tcp-ledbat_flexible_dumbell.cc
@@ -28,7 +28,11 @@ N_l_n- - - - - -                                    - - - - - - - N_r_n'
  * ./waf --run wifi-multirate
  *
  * To compile with command line(useful for varying parameters):
- * ./waf --run "<this file name> --simulationTime=0.3s --rateAdaptionAlgo=minstreal"
+ * ./waf --run "<this file name> --simulationTime=0.3s --rateAdaptionAlgo=ns3::MinstrelWifiManager"
+ *
+ * To turn on NS_LOG:
+ * export NS_LOG=multirate=level_all
+ * (can only view log if built with ./waf -d debug configure)
  *
  * To debug:
  * ./waf --shell
@@ -80,7 +84,7 @@ N_l_n- - - - - -                                    - - - - - - - N_r_n'
 using namespace ns3;
 
 
-NS_LOG_COMPONENT_DEFINE ("Evaluation of TCP-LEDBAT For Flexible Dumbell Topology");
+NS_LOG_COMPONENT_DEFINE ("Evaluation of TCP-LEDBAT For Flexible DUmbell Topology");
 
 
 
@@ -199,11 +203,12 @@ ConfigureRemoteStationManager (WifiHelper &wifiHelper, std::string rateAdaptionA
     }
  else
     {      
-    NS_LOG_DEBUG ("arf");
-    std::string phyRate = "HtMcs7"; 
-    wifiHelper.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
-                                        "DataMode", StringValue (phyRate),
-                                        "ControlMode", StringValue ("HtMcs0"));
+    NS_LOG_DEBUG ("constant-rate");
+    //std::string phyRate = "HtMcs7"; 
+    //wifiHelper.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+    //                                    "DataMode", StringValue (phyRate),
+    //                                    "ControlMode", StringValue ("HtMcs0"));
+    wifiHelper.SetRemoteStationManager ("ns3::ConstantRateWifiManager");
     return ;
     
     } 
@@ -221,7 +226,7 @@ ThroughputCalculation (double samplingPeriod, uint64_t minimaBetweenLeftAndRight
     * Convert Application RX Packets to MBits. and finally taking throughput sampling 
     * at each given sample period  
     */      
-    double newsamplingPeriod = samplingPeriod/1000;           
+    double newsamplingPeriod = samplingPeriod/1000;   // Converting sampling period from milisec to sec.         
     double currentFlow = ( (sink[nodeNumber]->GetTotalRx () - lastTotalRx[nodeNumber]) * (double) 8 /1e6 ) / (double) newsamplingPeriod;  
     aggregate += currentFlow ;
     if(nodeNumber < MAXOUTFILE + 1)
@@ -351,7 +356,6 @@ int main (int argc, char *argv[])
   Ipv4InterfaceContainer m_CentralLinkInterfaces;         // < Central Links AP0---AP1 interfaces (IPv4)
   WifiHelper wifi;
   MobilityHelper mobility;
-//   NodeContainer ap;
   WifiMacHelper wifiMac;
   NodeContainer wifiApNode0;
   NetDeviceContainer wifiApDevice0;
@@ -379,58 +383,84 @@ int main (int argc, char *argv[])
 
 
   /* Set up Legacy Channel , Keeping it same for both side dumbell*/
-  YansWifiChannelHelper wifiChannel;
+  YansWifiChannelHelper wifiChannel0;
   if( propagationDelayModel == "ConstantSpeedPropagationDelayModel" )
-        wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+        wifiChannel0.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
   else
-        wifiChannel.SetPropagationDelay ("ns3::" + propagationDelayModel );
+        wifiChannel0.SetPropagationDelay ("ns3::" + propagationDelayModel );
 
   if( propagationLossModel == "FriisPropagationLossModel" ) 
-        wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel", "Frequency", DoubleValue (5e9));
+        wifiChannel0.AddPropagationLoss ("ns3::FriisPropagationLossModel", "Frequency", DoubleValue (5e9));
   else
-        wifiChannel.AddPropagationLoss ("ns3::" + propagationLossModel );
+        wifiChannel0.AddPropagationLoss ("ns3::" + propagationLossModel );
 
 
   /* Setup Physical Layer , Keeping it same for both side dumbell */
-  YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
-  wifiPhy.SetChannel (wifiChannel.Create ());
+  YansWifiPhyHelper wifiPhy0 = YansWifiPhyHelper::Default ();
+  wifiPhy0.SetChannel (wifiChannel0.Create ());
   if( errorRateModel == "YansErrorRateModel" )
-        wifiPhy.SetErrorRateModel ("ns3::YansErrorRateModel");
+        wifiPhy0.SetErrorRateModel ("ns3::YansErrorRateModel");
   else
-        wifiPhy.SetErrorRateModel ("ns3::" + errorRateModel);
-//   //wifiPhy.SetErrorRateModel ("ns3::DsssErrorRateModel");
+        wifiPhy0.SetErrorRateModel ("ns3::" + errorRateModel);
+// wifiPhy.SetErrorRateModel ("ns3::DsssErrorRateModel");
 
 
  Ssid ssid0 = Ssid ("WTLedbat-EE-FD0"); // wifi tcp ledbat exhaustive evaluation on flexible dumbell BSS0
+
+
+  /* Set up Legacy Channel , Keeping it same for both side dumbell*/
+  YansWifiChannelHelper wifiChannel1;
+  if( propagationDelayModel == "ConstantSpeedPropagationDelayModel" )
+        wifiChannel1.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  else
+        wifiChannel1.SetPropagationDelay ("ns3::" + propagationDelayModel );
+
+  if( propagationLossModel == "FriisPropagationLossModel" ) 
+        wifiChannel1.AddPropagationLoss ("ns3::FriisPropagationLossModel", "Frequency", DoubleValue (5e9));
+  else
+        wifiChannel1.AddPropagationLoss ("ns3::" + propagationLossModel );
+
+
+  /* Setup Physical Layer , Keeping it same for both side dumbell */
+  YansWifiPhyHelper wifiPhy1 = YansWifiPhyHelper::Default ();
+  wifiPhy1.SetChannel (wifiChannel1.Create ());
+  if( errorRateModel == "YansErrorRateModel" )
+        wifiPhy1.SetErrorRateModel ("ns3::YansErrorRateModel");
+  else
+        wifiPhy1.SetErrorRateModel ("ns3::" + errorRateModel);
+// wifiPhy.SetErrorRateModel ("ns3::DsssErrorRateModel");
+
+
  Ssid ssid1 = Ssid ("WTLedbat-EE-FD1"); // wifi tcp ledbat exhaustive evaluation on flexible dumbell BSS1
 
  ConfigureWifiStandard ( wifi , wifiStandard );
  ConfigureRemoteStationManager ( wifi, rateAdaptionAlgo );
 
 
-
-
   // setup stas for Left Dumbell .
   wifiMac.SetType ("ns3::StaWifiMac",
                    "ActiveProbing", BooleanValue (true),
                    "Ssid", SsidValue (ssid0));
-  m_leftDumbellDevices = wifi.Install (wifiPhy, wifiMac, m_leftDumbellSTANode );
+
+
+  m_leftDumbellDevices = wifi.Install (wifiPhy0, wifiMac, m_leftDumbellSTANode );
 
   // setup stas for Right Dumbell .
   wifiMac.SetType ("ns3::StaWifiMac",
                    "ActiveProbing", BooleanValue (true),
-                   "Ssid", SsidValue (ssid0));
-  m_rightDumbellDevices = wifi.Install (wifiPhy, wifiMac, m_rightDumbellSTANode );
+                   "Ssid", SsidValue (ssid1));
+  m_rightDumbellDevices = wifi.Install (wifiPhy1, wifiMac, m_rightDumbellSTANode );
+
 
   // setup Left ap --> AP0.
   wifiMac.SetType ("ns3::ApWifiMac",
                    "Ssid", SsidValue (ssid0));
-  wifiApDevice0  = wifi.Install (wifiPhy, wifiMac, wifiApNode0);
+  wifiApDevice0  = wifi.Install (wifiPhy0, wifiMac, wifiApNode0);
 
   // setup Left ap --> AP1.
   wifiMac.SetType ("ns3::ApWifiMac",
-                   "Ssid", SsidValue (ssid0));
-  wifiApDevice1 = wifi.Install (wifiPhy, wifiMac, wifiApNode1);
+                   "Ssid", SsidValue (ssid1));
+  wifiApDevice1 = wifi.Install (wifiPhy1, wifiMac, wifiApNode1);
   
   
   // mobility configuration for left dumbell STAs.
@@ -445,15 +475,21 @@ int main (int argc, char *argv[])
   //Mobility Model
   if (mobilityModel == "RandomWalk2dMobilityModel" )
     mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",  
-                                "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
+                                "Bounds", RectangleValue (Rectangle (-40, 40, -40, 40)));
   else if( mobilityModel == "RandomDirection2dMobilityModel" )
-    mobility.SetMobilityModel ("ns3::RandomDirection2dMobilityModel");
+  {
+    mobility.SetMobilityModel ("ns3::RandomDirection2dMobilityModel",
+                                    "Bounds", RectangleValue (Rectangle (-70, 50, -70, 50)));
+  }
   else
     mobility.SetMobilityModel ("ns3::" + mobilityModel );
   
   mobility.Install (m_leftDumbellSTANode);
  
   //AP0 Mobility
+  Ptr<ListPositionAllocator> positionAllocForAP0 = CreateObject<ListPositionAllocator> ();
+  positionAllocForAP0->Add (Vector ( 10.0, 40.0, 0.0 ));
+  mobility.SetPositionAllocator( positionAllocForAP0 );
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobility.Install (wifiApNode0);
 
@@ -467,29 +503,34 @@ int main (int argc, char *argv[])
 //                                  "GridWidth", UintegerValue (1),
 //                                  "LayoutType", StringValue ("RowFirst"));
 
+  // mobility configuration for right dumbell STAs.
   Ptr<ListPositionAllocator> positionAllocRightDumbell = CreateObject<ListPositionAllocator> ();
   for (uint32_t nodeNumber = 0; nodeNumber < m_rightDumbellSTANode.GetN (); ++nodeNumber )
    {      
         Ptr<UniformRandomVariable> randomNumber = CreateObject<UniformRandomVariable> ();
-        randomNumber->SetAttribute ("Min", DoubleValue (5.0));
-        randomNumber->SetAttribute ("Max", DoubleValue (15.0));
-        positionAllocRightDumbell->Add (Vector (100.0 + 5.0*nodeNumber + (double) randomNumber->GetInteger (), 100.0 + 5.0*nodeNumber + (double) randomNumber->GetInteger (), 0.0));
+        randomNumber->SetAttribute ("Min", DoubleValue (10.0));
+        randomNumber->SetAttribute ("Max", DoubleValue (25.0));
+        positionAllocRightDumbell->Add (Vector (55.0 - 5.0*nodeNumber + (double) randomNumber->GetInteger (), 55.0 - 5.0*nodeNumber + (double) randomNumber->GetInteger (), 0.0));
    }
   mobility.SetPositionAllocator (positionAllocRightDumbell);
   
-  //Mobility Model
+  //Setting Mobility Model for right Dumbell STAs
   if (mobilityModel == "RandomWalk2dMobilityModel" )
     mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",  
-                                "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
+                                "Bounds", RectangleValue (Rectangle (-70, 50, -70, 50)));
   else if( mobilityModel == "RandomDirection2dMobilityModel" )
     mobility.SetMobilityModel ("ns3::RandomDirection2dMobilityModel",
-                                "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
+                                "Bounds", RectangleValue (Rectangle (-70, 50, -70, 50)));
   else
     mobility.SetMobilityModel ("ns3::" + mobilityModel );
 
   mobility.Install (m_rightDumbellSTANode);
   
+
   //AP1 Mobility
+  Ptr<ListPositionAllocator> positionAllocForAP1 = CreateObject<ListPositionAllocator> ();
+  positionAllocForAP1->Add (Vector ( 5.0, 70.0, 0.0 ));
+  mobility.SetPositionAllocator( positionAllocForAP1 );
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobility.Install (wifiApNode1);
 
@@ -500,8 +541,14 @@ int main (int argc, char *argv[])
 
   InternetStackHelper stack;
   stack.Install (m_accessPoints);
+  // stack.Install (wifiApNode0);
+  // stack.Install (wifiApNode1);
+  Ipv4StaticRoutingHelper staticRoutingHelper;
+  stack.SetRoutingHelper (staticRoutingHelper);
   stack.Install (m_leftDumbellSTANode);
   stack.Install (m_rightDumbellSTANode);
+
+
 
   //Assigns IPAddress to P2P Central DS Backbone
   Ipv4AddressHelper centralLinkIP;
@@ -514,24 +561,65 @@ int main (int argc, char *argv[])
   // Assign IP address to left side 
   leftIp.SetBase("10.1.2.0","255.255.255.0");
   NetDeviceContainer tempNdc0;
-  tempNdc0.Add (m_leftDumbellDevices);
-  tempNdc0.Add (m_rightDumbellDevices);
   tempNdc0.Add(wifiApDevice0);
-  tempNdc0.Add(wifiApDevice1);
- 
+  //tempNdc0.Add(wifiApDevice1);
+  tempNdc0.Add (m_leftDumbellDevices);
+  //tempNdc0.Add (m_rightDumbellDevices);
   Ipv4InterfaceContainer iifc0 = leftIp.Assign (tempNdc0);
-   for (uint32_t nodeNumber = 0; nodeNumber < m_leftDumbellSTANode.GetN (); ++nodeNumber )
+
+  for (uint32_t nodeNumber = 0; nodeNumber < m_leftDumbellSTANode.GetN (); ++nodeNumber )
    {
-      m_leftDumbellInterfaces.Add (iifc0.Get (nodeNumber));
+      m_leftDumbellInterfaces.Add (iifc0.Get (1+nodeNumber));
+      // std::cout<<"Node Number :"<< nodeNumber + 1 <<" -> "<<iifc0.Get (1+nodeNumber)<<std::endl;
     }
 //   Ipv4InterfaceContainer iifc0 = leftIp.Assign (tempNdc0);
-   for (uint32_t nodeNumber =  m_leftDumbellSTANode.GetN (); nodeNumber < m_leftDumbellSTANode.GetN () +m_rightDumbellSTANode.GetN (); ++nodeNumber )
-   {
-      m_rightDumbellInterfaces.Add (iifc0.Get (nodeNumber));
+  //  for (uint32_t nodeNumber =  m_leftDumbellSTANode.GetN (); nodeNumber < m_leftDumbellSTANode.GetN () +m_rightDumbellSTANode.GetN (); ++nodeNumber )
+  //  {
+  //     m_rightDumbellInterfaces.Add (iifc0.Get (nodeNumber));
+  //   }
+  // m_leftAPInterfaces.Add ( iifc0.Get (-1 + m_leftDumbellSTANode.GetN () + m_rightDumbellSTANode.GetN () ));
+
+  NodeContainer::Iterator iter;
+  for ( iter = m_leftDumbellSTANode.Begin (); iter != m_leftDumbellSTANode.End (); iter++)
+    {
+      Ptr<Ipv4StaticRouting> staticRouting;
+      staticRouting = Ipv4RoutingHelper::GetRouting <Ipv4StaticRouting> ((*iter)->GetObject<Ipv4> ()->GetRoutingProtocol ());
+      staticRouting->SetDefaultRoute ("10.1.2.1", 1 );
     }
-  m_leftAPInterfaces.Add ( iifc0.Get (-1 + m_leftDumbellSTANode.GetN () + m_rightDumbellSTANode.GetN () ));
-  m_rightAPInterfaces.Add ( iifc0.Get  ( m_leftDumbellSTANode.GetN () + m_rightDumbellSTANode.GetN () ));
+
+  m_leftAPInterfaces.Add ( iifc0.Get (0));
+  // std::cout<<"Node Number :"<< 0 <<" -> "<<iifc0.Get (0)<<std::endl;
+
   leftIp.NewNetwork ();
+
+
+ //==================================================================================================
+
+
+  // Assign IP address to right side 
+  rightIp.SetBase("10.3.1.0","255.255.255.0");
+  NetDeviceContainer tempNdc1;
+  tempNdc1.Add(wifiApDevice1);
+  tempNdc1.Add (m_rightDumbellDevices);
+  Ipv4InterfaceContainer iifc1 = rightIp.Assign (tempNdc1);
+
+   for (uint32_t nodeNumber = 0; nodeNumber < m_rightDumbellSTANode.GetN (); ++nodeNumber )
+   {
+      m_rightDumbellInterfaces.Add ( iifc1.Get ( 1+nodeNumber) );
+      // std::cout<<"Node Number :"<< nodeNumber + 1 <<" -> "<<iifc1.Get (1+nodeNumber)<<std::endl;
+
+    }
+
+  for ( iter = m_rightDumbellSTANode.Begin (); iter != m_rightDumbellSTANode.End (); iter++)
+    {
+      Ptr<Ipv4StaticRouting> staticRouting;
+      staticRouting = Ipv4RoutingHelper::GetRouting <Ipv4StaticRouting> ((*iter)->GetObject<Ipv4> ()->GetRoutingProtocol ());
+      staticRouting->SetDefaultRoute ("10.3.1.1", 1 );
+    }
+  m_rightAPInterfaces.Add ( iifc1.Get (0));
+  // std::cout<<"Node Number :"<< 0 <<" -> "<<iifc1.Get (0)<<std::endl;
+
+  rightIp.NewNetwork ();
 
 //   // Assign IP address to right side 
 //   rightIp.SetBase("10.1.3.0","255.255.255.0");
@@ -590,8 +678,8 @@ int main (int argc, char *argv[])
    std::ostringstream tempTss;
    tempTss << "Combine-Throughput-Flow.plt";
    allThroughputPlt.open (tempTss.str().c_str(), std::ofstream::out);
-   allThroughputPlt<< "set terminal pdf" <<"\n";
-   allThroughputPlt<< "set output \"" << "Combine-Throughput-Flow.pdf" <<"\"\n"; 
+   allThroughputPlt<< "set terminal png" <<"\n";
+   allThroughputPlt<< "set output \"" << "Combine-Throughput-Flow.png" <<"\"\n"; 
    allThroughputPlt<< "set title \"" << "Throghput of All "+ std::to_string( std::min( MAXOUTFILE, minimaBetweenLeftAndRightSide ) )+ " Flows" << "\"\n";
    allThroughputPlt<< "set xlabel \"TimeStamps\"\n";
    allThroughputPlt<< "set ylabel \"Throughput\"\n\n";
@@ -614,15 +702,19 @@ int main (int argc, char *argv[])
   /* Enable Traces */
   if (pcapTracing)
     {
-      wifiPhy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
-      wifiPhy.EnablePcap ("Wl-Fd-AccessPoint", m_accessPointsDevices);
-      wifiPhy.EnablePcap ("Wl-Fd-Station", m_rightDumbellDevices);
+      wifiPhy0.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
+      wifiPhy0.EnablePcap ("Wl-Fd-AccessPoint", m_accessPointsDevices.Get (0)); //wiif flexible dumbell
+      wifiPhy0.EnablePcap ("Wl-Fd-Station", m_leftDumbellDevices); //wiif flexible dumbell
+      wifiPhy1.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
+      wifiPhy1.EnablePcap ("Wl-Fd-AccessPoint", m_accessPointsDevices.Get (1)); //wiif flexible dumbell
+      wifiPhy1.EnablePcap ("Wl-Fd-Station", m_rightDumbellDevices); //wiif flexible dumbell
     }
 
  if (enableTracing)
     {
       AsciiTraceHelper ascii;
-      wifiPhy.EnableAsciiAll (ascii.CreateFileStream (GetOutputFileName () + ".tr"));
+      wifiPhy0.EnableAsciiAll (ascii.CreateFileStream ( (GetOutputFileName () + "_Phy0.tr").c_str()) );
+      wifiPhy1.EnableAsciiAll (ascii.CreateFileStream ( (GetOutputFileName () + "_Phy1.tr").c_str()) );
     }
 
   FlowMonitorHelper flowmonHelper;
